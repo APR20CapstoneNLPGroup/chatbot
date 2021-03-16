668432e5b6a7db11448e1249a6998b6263009bb0
APR20CapstoneNLPGroup <80706725+APR20CapstoneNLPGroup@users.noreply.github.com>
2021-03-16 09:55:31 +0530

    Update app.py

--- Milestone 3/app.py
+++ Milestone 3/app.py
@@ -10,6 +10,26 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('/index.html')
 
+@app.route('/predict',methods=['POST'])
+
+def predict():
+	
+	int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    return render_template('index.html', prediction_text="The Accident Severity is: {}".format(prediction))
+
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
