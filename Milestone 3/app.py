5759cc06f562e8aba7e83ef588040901f1b8e1c6
APR20CapstoneNLPGroup <80706725+APR20CapstoneNLPGroup@users.noreply.github.com>
2021-03-20 09:15:27 +0530

    Update app.py

--- Milestone 3/app.py
+++ Milestone 3/app.py
@@ -10,8 +10,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('/index.html')
 
-@app.route('/predict',methods=['POST'])
-
+#@app.route('/predict',methods=['POST'])
 def predict():
 	
 	int_features = [int(x) for x in request.form.values()]
